3a155d6dcb61742f4f6b95b0966ec225b904f84b
dhdessoldi <dhdessoldi@hotmail.com>
2023-01-25 16:17:04 -0300

    projeto iniciado

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner () {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Imagem com figuras geométricas e pessoas tocando as mãos"/>
+        </header>
+    )
+}
+
+export default Banner
